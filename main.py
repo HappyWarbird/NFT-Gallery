8350179f2a0c406ee763bf0032dced5b96e6afbd
HappyWarbird <happy_warbird@gmx.net>
2025-04-21 20:18:01 +0200

    added image resize for all formats

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import requests, mimetypes, datetime
+import requests, mimetypes, datetime, io
+from PIL import Image
 from pathlib import Path
 from config import ALCHEMY_API_KEY, ALCHEMY_CHAINS, WALLET_ADDRESS
 
 baseDir = Path("data")
 mainLog = Path("data/main.log")
 
+### Getting owned NFT Metadata from Alchemy NFT-API
 def getNftData(walletAddress, chain):
     print("Getting NFT Data for: " + walletAddress + " on " + chain)
     logHandler.write("Getting NFTs for: " + walletAddress + " on " + chain + "\n")
@@ -22,6 +24,7 @@ def getNftData(walletAddress, chain):
                           "collectionName": t["contract"]["openSeaMetadata"]["collectionName"],
                           "contract": t["contract"]["address"],
                           "tokenID": t["tokenId"]})
+        #Pagination Handling, preventing identical pageKey Bug
         if data.get("pageKey") is not None:
             if data.get("pageKey") == pageKey:
                 logHandler.write("[Error] Identical pageKey found for : " + walletAddress + " on " + chain + "\n")
@@ -34,6 +37,7 @@ def getNftData(walletAddress, chain):
             hasNext = pageKey is not None
     return assetData
 
+### Getting binary Image/Video Data and Filetype
 def imageLoader(imgURL):
     logHandler.write("Getting Image Data from: " + imgURL + "\n")
     imgData = {}
@@ -56,7 +60,23 @@ def imageLoader(imgURL):
                 imgData["fileExt"] = mimetypes.guess_extension(contentType, strict=True)
                 return imgData
 
+### Resizing image to maxsize relative 1080x1080
+def imageResizer(imageData):
+    image = Image.open(io.BytesIO(imageData))
+    if image.width == image.height:
+        image.resize((1080, 1080))
+        return image
+    else:
+        width, height = image.size
+        if width > height:
+            image.resize((1080, round(1080/width*height)))
+            return image
+        else:
+            image.resize((round(1080/height*width), 1080))
+            return image
+
 with open(mainLog, "a") as logHandler:
+    #Log Handling
     logHandler.write("Starting Image Loader " + datetime.datetime.now().strftime("%d.%m.%y %H:%M:%S") + "\n")
     for chain in ALCHEMY_CHAINS:
         assetData = getNftData(WALLET_ADDRESS, chain)
@@ -68,11 +88,15 @@ with open(mainLog, "a") as logHandler:
                 asset["imgData"] = imageLoader(asset["imgURL"])
                 print("Processing data for " + asset["collectionName"] + " " + asset["tokenID"])
                 if asset["imgData"]["fileExt"] in [".jpg", ".png"]:
-                    logHandler.write("Processing image data for " + asset["contract"] + " " + asset["tokenID"] + "\n")
                     #Handling of imagedata
+                    logHandler.write("Resizing image data for " + asset["contract"] + " " + asset["tokenID"] + "\n")
+                    image = imageResizer(asset["imgData"]["binary"])
+
                 elif asset["imgData"]["fileExt"] in [".gif"]:
-                    logHandler.write("Processing video data for " + asset["contract"] + " " + asset["tokenID"] + "\n")
                     #Handling of videodata
+                    logHandler.write("Processing video data for " + asset["contract"] + " " + asset["tokenID"] + "\n")
+                    
                 else:
+                    #Can't handle that stuff...
                     logHandler.write("Couldn't handle image data for " + asset["contract"] + " " + asset["tokenID"] + "\n")
                     continue
